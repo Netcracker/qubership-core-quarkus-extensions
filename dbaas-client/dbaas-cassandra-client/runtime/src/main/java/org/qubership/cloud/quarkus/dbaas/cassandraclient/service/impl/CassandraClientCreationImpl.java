2d509bc238eacf0b823f2bd2bf6383f41b3daceb
Artem Kuleshov <artem.kuleshov@netcracker.com>
2025-07-01 13:22:26 +0400

    feature: add new database-settings.replication setting for Cassandra DB

--- dbaas-client/dbaas-cassandra-client/runtime/src/main/java/org/qubership/cloud/quarkus/dbaas/cassandraclient/service/impl/CassandraClientCreationImpl.java
+++ dbaas-client/dbaas-cassandra-client/runtime/src/main/java/org/qubership/cloud/quarkus/dbaas/cassandraclient/service/impl/CassandraClientCreationImpl.java
@@ -1,21 +1,22 @@
 package org.qubership.cloud.quarkus.dbaas.cassandraclient.service.impl;
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.qubership.cloud.dbaas.client.cassandra.entity.connection.CassandraDBConnection;
 import org.qubership.cloud.dbaas.client.cassandra.entity.database.CassandraDatabase;
 import org.qubership.cloud.dbaas.client.cassandra.service.CassandraLogicalDbProvider;
 import org.qubership.cloud.dbaas.client.management.DatabaseConfig;
 import org.qubership.cloud.dbaas.client.management.DbaasDbClassifier;
 import org.qubership.cloud.dbaas.common.postprocessor.PostConnectProcessorManager;
+import org.qubership.cloud.quarkus.dbaas.cassandraclient.config.properties.CassandraDatabaseSettings;
 import org.qubership.cloud.quarkus.dbaas.cassandraclient.config.properties.CassandraDbConfiguration;
 import org.qubership.cloud.quarkus.dbaas.cassandraclient.config.properties.CassandraProperties;
 import org.qubership.cloud.quarkus.dbaas.cassandraclient.service.CassandraClientCreation;
 import org.qubership.cloud.quarkus.dbaas.cassandraclient.service.CqlSessionCreator;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Instance;
-import jakarta.inject.Inject;
-import lombok.extern.slf4j.Slf4j;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.Comparator;
 import java.util.List;
@@ -88,6 +89,8 @@ public class CassandraClientCreationImpl implements CassandraClientCreation {
         DatabaseConfig.Builder config = DatabaseConfig.builder();
         if (dbConfiguration != null) {
             config.physicalDatabaseId(dbConfiguration.getPhysicalDatabaseId().orElse(null));
+            CassandraDatabaseSettings databaseSettings = dbConfiguration.getDatabaseSettings();
+            config.databaseSettings(databaseSettings.isEmpty() ? null : databaseSettings.toDTO());
         }
         config.dbNamePrefix(cassandraProperties.getCassandraDbCreationConfig().
                 dbaasApiPropertiesConfig.getDbaaseApiProperties().getDbPrefix());
